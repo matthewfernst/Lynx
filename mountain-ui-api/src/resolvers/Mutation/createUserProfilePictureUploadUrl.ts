7fa2f57eae93c6e2aa669ad6c31fdac853a4ca4d
Max Rosoff <me@maxrosoff.com>
2023-04-25 17:58:36 -0700

    Move Bucket To Correct Region

--- mountain-ui-api/src/resolvers/Mutation/createUserProfilePictureUploadUrl.ts
+++ mountain-ui-api/src/resolvers/Mutation/createUserProfilePictureUploadUrl.ts
@@ -2,12 +2,16 @@ import { createSignedUploadUrl } from "../../aws/s3";
 import { Context } from "../../index";
 import { checkIsLoggedIn } from "../../auth";
 
-const fromBucket = "mountain-ui-users-profile-pictures";
+const fromBucket = "mountain-ui-app-profile-pictures";
 
-interface Args {
-}
+interface Args {}
 
-const createUserProfilePictureUploadUrl = async (_: any, args: Args, context: Context, info: any) => {
+const createUserProfilePictureUploadUrl = async (
+    _: any,
+    args: Args,
+    context: Context,
+    info: any
+) => {
     await checkIsLoggedIn(context);
     return createSignedUploadUrl(fromBucket, `${context.userId}-profile-picture`);
 };
