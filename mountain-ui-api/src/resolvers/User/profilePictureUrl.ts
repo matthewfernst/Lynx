0ac017e9bbf74c825ca5e672665d8b0d1c0655ad
Max Rosoff <me@maxrosoff.com>
2023-04-30 21:44:45 -0700

    Fix Profile Picture Aquisition Logic

--- mountain-ui-api/src/resolvers/User/profilePictureUrl.ts
+++ mountain-ui-api/src/resolvers/User/profilePictureUrl.ts
@@ -8,10 +8,10 @@ const profilePictureUrl = async (
     context: Context,
     info: any
 ): Promise<string | null> => {
-    if (parent.profilePictureUrl) {
-        return parent.profilePictureUrl;
-    } else if (await checkIfObjectInBucket(profilePictureBucketName, parent.id)) {
+    if (await checkIfObjectInBucket(profilePictureBucketName, parent.id)) {
         return `https://${profilePictureBucketName}.s3.us-west-1.amazonaws.com/${parent.id}`;
+    } else if (parent.profilePictureUrl) {
+        return parent.profilePictureUrl;
     } else {
         return null;
     }
