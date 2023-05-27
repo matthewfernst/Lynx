104ae6b2ead5429d96fe62b603f5282ca4e3fea9
Max Rosoff <me@maxrosoff.com>
2023-05-27 22:35:36 +0800

    Add More Debugging Messages

--- mountain-ui-api/src/aws/s3.ts
+++ mountain-ui-api/src/aws/s3.ts
@@ -37,13 +37,13 @@ export const checkIfObjectInBucket = async (bucketName: string, path: string) =>
     }
 };
 
-export const getRecordsFromBucket = async (bucketName: string, path: string = "") => {
+export const getRecordsFromBucket = async (bucketName: string, prefix: string = "") => {
     const s3Client = createS3Client();
     try {
-        const listObjectsRequest = new ListObjectsCommand({ Bucket: bucketName, Prefix: path });
+        const listObjectsRequest = new ListObjectsCommand({ Bucket: bucketName, Prefix: prefix });
         const listObjectsResponse = await s3Client.send(listObjectsRequest);
         if (!listObjectsResponse.Contents) {
-            throw new Error("Error retrieving contents of bucket");
+            throw new Error("Error retrieving info of items in bucket");
         }
         return await Promise.all(
             listObjectsResponse.Contents.map(async (content) => {
@@ -53,13 +53,13 @@ export const getRecordsFromBucket = async (bucketName: string, path: string = ""
                 });
                 const getObjectResponse = await s3Client.send(getObjectRequest);
                 if (!getObjectResponse.Body) {
-                    throw new Error("Error retrieving contents of bucket");
+                    throw new Error("Error reading information about item in bucket");
                 }
                 return await getObjectResponse.Body.transformToString();
             })
         );
     } catch (err) {
         console.error(err);
-        throw Error("Error retrieving contents of bucket");
+        throw Error("Error retrieving records from bucket with prefix");
     }
 };
