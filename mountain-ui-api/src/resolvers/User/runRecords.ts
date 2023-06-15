6fddd525e45b43f79c45ba47feced43af83b8ad8
Max Rosoff <me@maxrosoff.com>
2023-06-15 09:24:22 -0700

    Add Types And Cleanup Function

--- mountain-ui-api/src/resolvers/User/runRecords.ts
+++ mountain-ui-api/src/resolvers/User/runRecords.ts
@@ -1,16 +1,18 @@
 import { Context } from "../../index";
 import { RunRecord } from "../../types";
 import { getRecordsFromBucket, toRunRecordsBucket } from "../../aws/s3";
-import { parseString } from 'xml2js';
+import { parseStringPromise } from "xml2js";
 
 const runRecords = async (
-  parent: any,
-  args: {},
-  context: Context,
-  info: any
+    parent: any,
+    args: {},
+    context: Context,
+    info: any
 ): Promise<RunRecord[]> => {
-  const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
-  return Promise.all(records.map(async (record) => await parseString(record) as RunRecord)):
+    const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
+    return Promise.all(
+        records.map(async (record) => (await parseStringPromise(record)) as RunRecord)
+    );
 };
 
 export default runRecords;
