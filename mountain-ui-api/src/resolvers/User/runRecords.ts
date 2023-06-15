cf3725122bddf17fd080e4eb2f506fa4d08a8bdc
Max Rosoff <me@maxrosoff.com>
2023-06-15 10:01:41 -0700

    Fix Up XML Parsing

--- mountain-ui-api/src/resolvers/User/runRecords.ts
+++ mountain-ui-api/src/resolvers/User/runRecords.ts
@@ -1,7 +1,7 @@
 import { Context } from "../../index";
 import { RunRecord } from "../../types";
 import { getRecordsFromBucket, toRunRecordsBucket } from "../../aws/s3";
-import { parseStringPromise } from "xml2js";
+import { parseStringPromise, processors } from "xml2js";
 
 const runRecords = async (
     parent: any,
@@ -11,7 +11,18 @@ const runRecords = async (
 ): Promise<RunRecord[]> => {
     const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
     return Promise.all(
-        records.map(async (record) => (await parseStringPromise(record)) as RunRecord)
+        records.map(async (record): Promise<RunRecord> => {
+            const xml: { activity: RunRecord } = await parseStringPromise(record, {
+                normalize: true,
+                mergeAttrs: true,
+                explicitArray: false,
+                tagNameProcessors: [processors.firstCharLowerCase],
+                attrNameProcessors: [processors.firstCharLowerCase],
+                valueProcessors: [processors.parseBooleans, processors.parseNumbers],
+                attrValueProcessors: [processors.parseBooleans, processors.parseNumbers]
+            });
+            return xml.activity;
+        })
     );
 };
 
