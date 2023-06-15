1d7fee271edeab174288141df371d0866f9d545d
Matthew Ernst <matthew.f.ernst@gmail.com>
2023-06-15 08:29:03 -0700

    addition of xml2json for run records (#61)
    Signed-off-by: matthewfernst <matthew.f.ernst@gmail.com>

--- mountain-ui-api/src/resolvers/User/runRecords.ts
+++ mountain-ui-api/src/resolvers/User/runRecords.ts
@@ -1,6 +1,7 @@
 import { Context } from "../../index";
 import { RunRecord } from "../../types";
 import { getRecordsFromBucket, toRunRecordsBucket } from "../../aws/s3";
+import { parseString } from 'xml2js';
 
 const runRecords = async (
     parent: any,
@@ -9,7 +10,22 @@ const runRecords = async (
     info: any
 ): Promise<RunRecord[]> => {
     const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
-    return records.map((record) => JSON.parse(record));
+    const parsedRecords: RunRecord[] = [];
+
+    // Parse each XML record to JSON
+    for (const record of records) {
+        parseString(record, (err, result) => {
+            if (err) {
+                console.error('Error parsing XML:', err);
+                return;
+            }
+            const jsonRecord = JSON.stringify(result);
+            parsedRecords.push(JSON.parse(jsonRecord));
+        });
+    }
+
+    return parsedRecords;
 };
 
 export default runRecords;
+
