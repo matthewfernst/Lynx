03d1648b120f7bbe4a6ce4768d38f7ab27c4237c
matthewfernst <matthew.f.ernst@gmail.com>
2023-06-15 09:11:17 -0700

    parse to xml
    Signed-off-by: matthewfernst <matthew.f.ernst@gmail.com>

--- mountain-ui-api/src/resolvers/User/runRecords.ts
+++ mountain-ui-api/src/resolvers/User/runRecords.ts
@@ -4,28 +4,13 @@ import { getRecordsFromBucket, toRunRecordsBucket } from "../../aws/s3";
 import { parseString } from 'xml2js';
 
 const runRecords = async (
-    parent: any,
-    args: {},
-    context: Context,
-    info: any
+  parent: any,
+  args: {},
+  context: Context,
+  info: any
 ): Promise<RunRecord[]> => {
-    const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
-    const parsedRecords: RunRecord[] = [];
-
-    // Parse each XML record to JSON
-    for (const record of records) {
-        parseString(record, (err, result) => {
-            if (err) {
-                console.error('Error parsing XML:', err);
-                return;
-            }
-            const jsonRecord = JSON.stringify(result);
-            parsedRecords.push(JSON.parse(jsonRecord));
-        });
-    }
-
-    return parsedRecords;
+  const records = await getRecordsFromBucket(toRunRecordsBucket, parent.id);
+  return Promise.all(records.map(async (record) => await parseString(record) as RunRecord)):
 };
 
 export default runRecords;
-
