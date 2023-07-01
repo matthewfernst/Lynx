deca8dbe1aeed832f844bd931b2605be6d1bcc88
Max Rosoff <me@maxrosoff.com>
2023-07-01 10:22:25 -0700

    Invite Keys Are Now 6 Digits

--- mountain-ui-api/src/resolvers/Mutation/createInviteKey.ts
+++ mountain-ui-api/src/resolvers/Mutation/createInviteKey.ts
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid";
 import { DateTime } from "luxon";
 
 import { Context } from "../../index";
@@ -8,7 +7,7 @@ import { DYNAMODB_TABLE_INVITES, putItem } from "../../aws/dynamodb";
 const createInviteKey = async (_: any, args: any, context: Context, info: any): Promise<string> => {
     await checkIsLoggedInAndHasValidToken(context);
     console.log(`Generating invite token for user with id ${context.userId}`);
-    const inviteKey = uuid();
+    const inviteKey = Math.random().toString(10).substring(2, 8);
     const ttl = DateTime.now().toSeconds() + 60 * 60 * 24;
     await putItem(DYNAMODB_TABLE_INVITES, { id: inviteKey, ttl });
     return inviteKey;
