693c528921c1d2a6217ca5b536574466edff9610
Max Rosoff <me@maxrosoff.com>
2023-07-06 08:45:00 -0500

    Fix OAuth Combine Mutation

--- mountain-ui-api/src/resolvers/Mutation/combineOAuthAccounts.ts
+++ mountain-ui-api/src/resolvers/Mutation/combineOAuthAccounts.ts
@@ -28,39 +28,23 @@ const combineOAuthAccounts = async (
     info: any
 ): Promise<User> => {
     checkIsLoggedInAndHasValidInvite(context);
-    if (args.combineWith.token) {
-        return await updateWithNewLogin(
-            context.userId as string,
-            args.combineWith.type,
-            args.combineWith.id,
-            args.combineWith.token
-        );
-    } else {
-        return await mergeExistingAccount(
-            context.userId as string,
-            args.combineWith.type,
-            args.combineWith.id
-        );
-    }
-};
-
-const updateWithNewLogin = async (userId: string, type: LoginType, id: string, token: string) => {
-    const idKey = idKeyFromIdType(type);
-    await verifyToken(type, id, token);
-    await updateItem(DYNAMODB_TABLE_USERS, userId, idKey, id);
-    const queryOutput = await getItem(DYNAMODB_TABLE_USERS, userId);
-    return getItemFromDynamoDBResult(queryOutput) as User;
-};
-
-const mergeExistingAccount = async (userId: string, type: LoginType, id: string) => {
+    const { type, id, token } = args.combineWith;
     const idKey = idKeyFromIdType(type);
     const userQuery = await getItemsByIndex(DYNAMODB_TABLE_USERS, idKey, id);
     const otherUser = getItemFromDynamoDBResult(userQuery) as User | null;
     if (!otherUser) {
-        throw new UserInputError("No Token Provided And User Does Not Exist");
+        if (!token) {
+            throw new UserInputError("User Does Not Exist and No Token Provided");
+        }
+        await verifyToken(type, id, token);
+        return await updateUserAndReturnResult(context.userId as string, idKey, id);
     }
-    await updateItem(DYNAMODB_TABLE_USERS, userId, idKey, id);
     await deleteItem(DYNAMODB_TABLE_USERS, otherUser.id);
+    return await updateUserAndReturnResult(context.userId as string, idKey, id);
+};
+
+const updateUserAndReturnResult = async (userId: string, idKey: string, id: string) => {
+    await updateItem(DYNAMODB_TABLE_USERS, userId, idKey, id);
     const queryOutput = await getItem(DYNAMODB_TABLE_USERS, userId);
     return getItemFromDynamoDBResult(queryOutput) as User;
 };
