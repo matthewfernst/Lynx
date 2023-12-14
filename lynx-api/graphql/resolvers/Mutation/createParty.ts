86effc6c9ee51021afae62fc2ccb33a2521fb0a1
Max Rosoff <me@maxrosoff.com>
2023-12-14 13:59:31 -0500

    Ensure UserDB is Updated with Parties

--- lynx-api/graphql/resolvers/Mutation/createParty.ts
+++ lynx-api/graphql/resolvers/Mutation/createParty.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from "uuid";
 
 import { Context } from "../../index";
 import { checkIsLoggedInAndHasValidInvite } from "../../auth";
-import { PARTIES_TABLE, putItem } from "../../aws/dynamodb";
+import { PARTIES_TABLE, USERS_TABLE, addItemsToArray, putItem } from "../../aws/dynamodb";
 import { Party } from "../../types";
 
 interface Args {
@@ -13,11 +13,13 @@ const createParty = async (_: any, args: Args, context: Context, info: any): Pro
     await checkIsLoggedInAndHasValidInvite(context);
     console.log(`Creating party token for user with id ${context.userId}`);
     const partyId = uuid();
-    return await putItem(PARTIES_TABLE, {
+    const party = await putItem(PARTIES_TABLE, {
         id: partyId,
         name: args.name,
         users: [context.userId]
     });
+    await addItemsToArray(USERS_TABLE, context.userId as string, "parties", [partyId]);
+    return party;
 };
 
 export default createParty;
