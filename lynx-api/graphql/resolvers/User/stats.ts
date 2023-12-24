3754661b449c52f582f0200b3f1dead2c7a477e1
Max Rosoff <me@maxrosoff.com>
2023-12-23 23:04:17 -0800

    Add Stats Resolver

---
+++ lynx-api/graphql/resolvers/User/stats.ts
@@ -0,0 +1,47 @@
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { GraphQLError } from "graphql";
+
+import { Context, DEPENDENCY_ERROR } from "../../index";
+import { UserStats } from "../../types";
+import { Timeframe, leaderboardTimeframeFromQueryArgument } from "../Query/leaderboard";
+import { LEADERBOARD_TABLE } from "../../../infrastructure/lib/infrastructure";
+import { documentClient } from "../../aws/dynamodb";
+
+interface Parent {
+    id: string;
+}
+
+interface Args {
+    timeframe: Timeframe;
+}
+
+const stats = async (
+    parent: Parent,
+    args: Args,
+    context: Context,
+    info: any
+): Promise<UserStats | undefined> => {
+    return await getUserStatsFromLeaderboard(
+        parent.id,
+        leaderboardTimeframeFromQueryArgument(args.timeframe)
+    );
+};
+
+const getUserStatsFromLeaderboard = async (id: string, timeframe: string): Promise<UserStats> => {
+    try {
+        console.log(`Getting user stats with timeframe ${timeframe}`);
+        const queryRequest = new GetCommand({
+            TableName: LEADERBOARD_TABLE,
+            Key: { id, timeframe }
+        });
+        const itemOutput = await documentClient.send(queryRequest);
+        return itemOutput.Item as UserStats;
+    } catch (err) {
+        console.error(err);
+        throw new GraphQLError("DynamoDB Query Call Failed", {
+            extensions: { code: DEPENDENCY_ERROR }
+        });
+    }
+};
+
+export default stats;
